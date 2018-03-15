7a8480af7bcc04091ea47def5e644b4e041f4542
liangjiajie <liangjiajie@qtong.cn>
2018-03-15 13:54:45 +0800

    新增showMsg方法，用于测试

--- js/controllers/school.js
+++ js/controllers/school.js
@@ -6,7 +6,11 @@ angular.module('lsApp')
         $scope.menuIndex = 0;
         $scope.menuClick = function(index){
             $scope.menuIndex = index;
-        }
+        };
+        $scope.showMsg = function(msg) {
+            var _msg = msg || 'hello_msg';
+            alert(_msg)
+        };
 
 
     }])
